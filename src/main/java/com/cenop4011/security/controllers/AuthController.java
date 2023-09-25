755857773367f8c3635125a88cfaf225357fdd8b
Jocimar Walter <f5078775@bb.com.br>
2023-09-25 10:42:04 -0300

    atualizado

--- src/main/java/com/cenop4011/security/controllers/AuthController.java
+++ src/main/java/com/cenop4011/security/controllers/AuthController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,6 +40,7 @@ import io.swagger.annotations.ApiImplicitParams;
 @Api(tags = "auth",description = " ")
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin("*")
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final PasswordEncoder passwordEncoder;
