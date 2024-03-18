a41eb520c36f1feef279164cd04a14bf37e62381
Jocimar Walter <f5078775@bb.com.br>
2024-03-18 09:25:20 -0300

    atualizado

--- src/main/java/com/cenop4011/padroniza/controllers/ComportamentoRespostaController.java
+++ src/main/java/com/cenop4011/padroniza/controllers/ComportamentoRespostaController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,9 +17,14 @@ import com.cenop4011.padroniza.dtos.ComportamentoRespostaDTO;
 import com.cenop4011.padroniza.models.ComportamentoResposta;
 import com.cenop4011.padroniza.services.ComportamentoRespostaService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+
 @RestController
 @RequestMapping("comportamentos")
 @CrossOrigin(allowCredentials = "true",originPatterns = "*")
+@Api(tags = "Gerenciar comportamento do valor da resposta",description = " ")
 public class ComportamentoRespostaController {
 	
 	
@@ -28,6 +34,11 @@ public class ComportamentoRespostaController {
 	
 	
 	@PostMapping("/{idResposta}")
+	
+	@ApiImplicitParams({
+	       @ApiImplicitParam(name = "Authorization", value = "Informe o token com Bearer no inicio", required = true, dataType = "string", paramType = "header")
+	})
+		@PreAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
 	public ResponseEntity<?> gravarAtualizarComportamento(@PathVariable Integer idResposta, @RequestBody ComportamentoRespostaDTO comportamentoRespostaDTO , HttpServletRequest req){
 		
 		
