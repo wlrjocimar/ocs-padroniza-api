7fcff1c487e9aeb91d9bc63013efdbb676f4deb7
Jocimar Walter <f5078775@bb.com.br>
2024-03-27 14:28:44 -0300

    atualizado

--- src/main/java/com/cenop4011/padroniza/repositories/PerguntaRepository.java
+++ src/main/java/com/cenop4011/padroniza/repositories/PerguntaRepository.java
@@ -18,7 +18,7 @@ public interface PerguntaRepository extends JpaRepository<Pergunta, Integer> {
 	
 	
 	
-	@Query(value="SELECT t1.* FROM ocs_padroniza_dev.tb_pergunta t0 inner join ocs_padroniza_dev.tb_bloco_pergunta t1 on t0.id=t1.pergunta_id inner join ocs_padroniza_dev.tb_checklist_bloco t2 on t1.bloco_id=t2.bloco_id  where t0.automatizavel is true and t2.checklist_id = ?1;",nativeQuery = true)
+	@Query(value="SELECT t0.* FROM ocs_padroniza_dev.tb_pergunta t0 inner join ocs_padroniza_dev.tb_bloco_pergunta t1 on t0.id=t1.pergunta_id inner join ocs_padroniza_dev.tb_checklist_bloco t2 on t1.bloco_id=t2.bloco_id  where t0.automatizavel is true and t2.checklist_id = ?1",nativeQuery = true)
 	List<Pergunta> findByAutomatizavelPorChecklist(@Param(value = "idChecklist") Integer idChecklist);
 	
 	
